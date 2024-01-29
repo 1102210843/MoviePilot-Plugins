e90a2fae1d3e237e549c93b2555ddbb9300f41c3
sunyu8 <sunyu8@wps.cn>
2024-01-29 11:03:01 +0800

    修改版本号

--- plugins/brushflowself/__init__.py
+++ plugins/brushflowself/__init__.py
@@ -31,7 +31,7 @@ class BrushFlowSelf(_PluginBase):
     # 插件图标
     plugin_icon = "brush.jpg"
     # 插件版本
-    plugin_version = "1.1"
+    plugin_version = "1.2"
     # 插件作者
     plugin_author = "Sunny"
     # 作者主页
